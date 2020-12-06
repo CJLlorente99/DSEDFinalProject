1f887bdc0710f1ff3fc7ca6bce291562e30b2089
Char Llorente Cortijo <cj.llorente@alumnos.upm.es>
2020-12-06 13:31:35 +0100

    Tarea 2.10

--- dsed_audio/dsed_audio.sim/sim_1/behav/elaborate.bat
+++ dsed_audio/dsed_audio.sim/sim_1/behav/elaborate.bat
@@ -1,6 +1,6 @@
 @echo off
 set xv_path=C:\\Xilinx\\Vivado\\2017.2\\bin
-call %xv_path%/xelab  -wto 57c794d15a2945ca9f5ce459fb299daf -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L secureip -L xpm --snapshot fir_filter_tb_behav xil_defaultlib.fir_filter_tb -log elaborate.log
+call %xv_path%/xelab  -wto 57c794d15a2945ca9f5ce459fb299daf -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L secureip -L xpm --snapshot fir_filter_2_tb_behav xil_defaultlib.fir_filter_2_tb -log elaborate.log
 if "%errorlevel%"=="0" goto SUCCESS
 if "%errorlevel%"=="1" goto END
 :END
